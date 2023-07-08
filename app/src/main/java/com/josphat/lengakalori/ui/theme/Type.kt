bfb6118cb9bcf283042074952d22bd8ed6cdf13b
josphatmwania <josphatmwania237@gmail.com>
2023-07-08 14:18:43 +0300

    Staging them-ui errors

--- app/src/main/java/com/josphat/lengakalori/ui/theme/Type.kt
+++ app/src/main/java/com/josphat/lengakalori/ui/theme/Type.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+//import androidx.compose.material3.MaterialTypography
+
 
 val Typography = Typography(
     body1 = TextStyle(
