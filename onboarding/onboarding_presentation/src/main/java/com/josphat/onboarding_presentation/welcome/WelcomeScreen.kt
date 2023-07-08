603c2c95a2ba3cc62e9112c93f35c63fb8c3295b
josphatmwania <josphatmwania237@gmail.com>
2023-07-08 15:08:18 +0300

    Add WelcomeScreen function

--- onboarding/onboarding_presentation/src/main/java/com/josphat/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/josphat/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -1,5 +1,33 @@
 package com.josphat.onboarding_presentation.welcome
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
+fun WelcomeScreen() {
+    /**
+     * Todo
+     *
+     */
+//    val spacing = LocalSpacing.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource()
+        )
+    }
+
+}
