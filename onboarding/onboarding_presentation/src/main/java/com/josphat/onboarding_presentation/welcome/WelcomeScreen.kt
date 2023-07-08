dc5a4b7f9fe8e7a5e7fdfe8b5b1108b1641a37cf
josphatmwania <josphatmwania237@gmail.com>
2023-07-08 15:55:28 +0300

    Create welcomeSCreen fun

--- onboarding/onboarding_presentation/src/main/java/com/josphat/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/josphat/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -2,14 +2,19 @@ package com.josphat.onboarding_presentation.welcome
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.josphat.onboarding_presentation.R
 
 @Composable
 fun WelcomeScreen() {
@@ -26,8 +31,11 @@ fun WelcomeScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource()
+            text = stringResource(id = R.string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.labelLarge
         )
+        Spacer(modifier = Modifier.height(24.dp))
     }
 
 }
