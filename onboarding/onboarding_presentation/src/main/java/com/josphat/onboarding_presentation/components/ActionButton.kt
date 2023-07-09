62d25c5d9dadedbea83fa99479f45d6bbf54a376
josphatmwania <josphatmwania237@gmail.com>
2023-07-09 18:45:53 +0300

    Add components & ActionButton fun

---
+++ onboarding/onboarding_presentation/src/main/java/com/josphat/onboarding_presentation/components/ActionButton.kt
@@ -0,0 +1,22 @@
+package com.josphat.onboarding_presentation.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import java.time.format.TextStyle
+
+@Composable
+fun ActionButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier,
+    isEnabled: Boolean = false,
+    /**
+     * Todo
+     */
+//    textStyle: TextStyle = MaterialTheme.typography.labelMedium
+) {
+
+}
+
+
